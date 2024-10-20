0b05767216ec534c05bafe1d36bc7dbfc1f50319
Osman M <osmanmunur@yahoo.com>
2024-10-20 15:47:48 +0100

    Initial Commit

---
+++ PEMF/PEMFApp.swift
@@ -0,0 +1,17 @@
+//
+//  PEMFApp.swift
+//  PEMF
+//
+//  Created by asia on 20.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct PEMFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
